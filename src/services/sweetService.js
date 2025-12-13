20c9f759ac0aeab0d004990b4706d258d82f9473
rachit012 <rachitthakur012@gmail.com>
2025-12-13 23:34:57 +0530

    refactor(sweets): simplify and normalize sweet search filtering

--- src/services/sweetService.js
+++ src/services/sweetService.js
@@ -3,16 +3,16 @@ const Sweet = require('../models/Sweet')
 exports.create = data => Sweet.create(data)
 exports.getAll = () => Sweet.find()
 
-exports.search = query => {
-  const filter = {}
-
-  if (query.name) filter.name = query.name
-  if (query.category) filter.category = query.category
-
-  if (query.minPrice || query.maxPrice) {
-    filter.price = {}
-    if (query.minPrice) filter.price.$gte = Number(query.minPrice)
-    if (query.maxPrice) filter.price.$lte = Number(query.maxPrice)
+exports.search = ({ name, category, minPrice, maxPrice }) => {
+  const filter = {
+    ...(name && { name }),
+    ...(category && { category }),
+    ...((minPrice || maxPrice) && {
+      price: {
+        ...(minPrice && { $gte: Number(minPrice) }),
+        ...(maxPrice && { $lte: Number(maxPrice) })
+      }
+    })
   }
 
   return Sweet.find(filter)
